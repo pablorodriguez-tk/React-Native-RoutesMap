fdcf00f9b78a3c9618777efe6d91e9892d9396ec
Pablo Rodriguez <pablorodriguez88@gmail.com>
2021-06-14 14:22:04 -0300

    Add isMounted to avoid problems

--- src/hooks/useLocation.tsx
+++ src/hooks/useLocation.tsx
@@ -15,9 +15,19 @@ export const useLocation = () => {
     latitude: 0,
   });
   const watchId = useRef<number>();
+  const isMounted = useRef<boolean>(true);
+
+  useEffect(() => {
+    isMounted.current = true;
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
 
   useEffect(() => {
     getCurrentLocation().then(location => {
+      if (!isMounted.current) return;
+
       setInitialPosition(location);
       setUserLocation(location);
       setRouteLines(routes => [...routes, location]);
@@ -43,6 +53,8 @@ export const useLocation = () => {
   const followUserLocation = () => {
     watchId.current = Geolocation.watchPosition(
       ({coords}) => {
+        if (!isMounted.current) return;
+
         const location: Location = {
           latitude: coords.latitude,
           longitude: coords.longitude,
